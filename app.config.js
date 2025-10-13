21aaf4949b4762f22f7c29afb49be3fb9d04f38e
Jordan Kulzer <jbkulzer@gmail.com>
2025-10-13 14:14:18 -0500

    version update 25-26

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Squares",
     slug: "squares",
-    version: "1.0.25",
+    version: "1.0.26",
     orientation: "portrait",
     icon: "./assets/icons/squares-logo-white.png",
     userInterfaceStyle: "automatic",
@@ -17,7 +17,7 @@ export default {
     },
     ios: {
       bundleIdentifier: "com.jkulzer.squaresgame",
-      buildNumber: "25",
+      buildNumber: "26",
       supportsTablet: true,
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
